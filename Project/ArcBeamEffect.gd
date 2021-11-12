afc917b1532cc8768200df42866e5737256f97db
AtkinTC <Atkintc@gmail.com>
2021-11-11 19:11:48 -0500

    modified ArcBeamEffect to use multiple separate beam lines
    Single bent Line2D resulted in weird visual distortions, changed to a separate Line2D for each segment

--- Project/ArcBeamEffect.gd
+++ Project/ArcBeamEffect.gd
@@ -1,6 +1,7 @@
 extends Effect
 
-onready var arc_beam: Line2D = get_node("ArcBeam")
+export var beam_scene: PackedScene
+var arc_beams: Array
 var duration_timer: Timer
 var source: Node2D
 var chain_targets: Array
@@ -13,16 +14,22 @@ func setup_from_attribute_dictionary(_attribute_dict: Dictionary):
 	duration = _attribute_dict.get("duration", -1)
 
 func _ready() -> void:
+	if(beam_scene == null):
+		queue_free()
+	
 	if(duration >= 0):
 		duration_timer = Timer.new()
 		duration_timer.set_one_shot(true)
 		duration_timer.connect("timeout", self, "_on_duration_timeout")
 		add_child(duration_timer)
 		duration_timer.start(duration)
+	
+	for i in chain_targets.size():
+		var beam_dup = beam_scene.instance()
+		add_child(beam_dup)
+		arc_beams.append(beam_dup)
+		
 	update_arc()
-
-#func _process(delta) -> void:
-#	pass
 
 func _physics_process(delta) -> void:
 	update_arc()
@@ -35,11 +42,24 @@ func update_arc():
 		return false
 	else:
 		self.visible = true
-		var line_points := [source.global_position]
-		for target in chain_targets:
+		#update positions in chain
+		var line_points = [source.global_position]
+		for i in chain_targets.size():
+			var target = chain_targets[i]
+			#if the target no longer exists, just leave the point where it is
 			if(target != null && is_instance_valid(target)):
 				line_points.append((target as Node2D).global_position)
-		arc_beam.set_points(line_points)
+		
+		#position one beam between each pair of points in the chain
+		for i in (line_points.size() - 1):
+			(arc_beams[i] as Line2D).set_points([line_points[i], line_points[i+1]])
+			
+		# clean up extra beams if the chain becomes shorter
+		for i in (arc_beams.size() - line_points.size() + 1):
+			var beam = arc_beams.pop_back()
+			beam.queue_free()
+			
+			
 
 func _on_duration_timeout() -> void:
 	self.visible = false
